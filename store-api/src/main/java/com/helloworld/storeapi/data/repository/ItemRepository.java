69d307c52a233bdbe22c6364b0f4bb3440a2bd28
Davide Valentino <daval302@gmail.com>
2020-06-28 19:58:59 +0100

    feat(): repository REST mappings

--- store-api/src/main/java/com/helloworld/storeapi/data/repository/ItemRepository.java
+++ store-api/src/main/java/com/helloworld/storeapi/data/repository/ItemRepository.java
@@ -3,6 +3,9 @@ package com.helloworld.storeapi.data.repository;
 import com.helloworld.storeapi.data.model.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ItemRepository extends JpaRepository<Item, Integer>
 {
+    Optional<Item> findById(Integer id);
 }
