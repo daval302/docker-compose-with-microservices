19caf5b3ed5d72c8fd92ba7960195d6a66218eee
Davide Valentino <daval302@gmail.com>
2020-06-30 21:07:43 +0100

    payment-api added to the docker-compose

--- store-api/src/main/java/com/helloworld/storeapi/data/model/Checkout.java
+++ store-api/src/main/java/com/helloworld/storeapi/data/model/Checkout.java
@@ -9,6 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
--- store-api/src/main/java/com/helloworld/storeapi/data/repository/CheckoutRepository.java
+++ store-api/src/main/java/com/helloworld/storeapi/data/repository/CheckoutRepository.java
@@ -1,6 +1,7 @@
 package com.helloworld.storeapi.data.repository;
 
 import com.helloworld.storeapi.data.model.Checkout;
+import com.helloworld.storeapi.data.model.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CheckoutRepository extends JpaRepository<Checkout, Integer>
