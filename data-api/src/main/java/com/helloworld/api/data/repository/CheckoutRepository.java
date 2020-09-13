dac68a8328328c145b23518ec8b265ea7c35a784
Davide Valentino <daval302@gmail.com>
2020-09-13 16:35:21 +0100

    payment-api receive requests and move PENDING to PAID at random rate of success/unseuccessiful result

--- data-api/src/main/java/com/helloworld/api/data/repository/CheckoutRepository.java
+++ data-api/src/main/java/com/helloworld/api/data/repository/CheckoutRepository.java
@@ -3,6 +3,9 @@ package com.helloworld.api.data.repository;
 import com.helloworld.api.data.model.Checkout;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CheckoutRepository extends JpaRepository<Checkout, Integer>
 {
+    List<Checkout> findAllByState(String state);
 }
--- payment-api/src/main/java/com/helloworld/paymentapi/controller/PaymentController.java
+++ payment-api/src/main/java/com/helloworld/paymentapi/controller/PaymentController.java
@@ -1,22 +1,37 @@
 package com.helloworld.paymentapi.controller;
 
+import com.helloworld.paymentapi.data.Checkout;
 import com.helloworld.paymentapi.data.PaymentRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.HashMap;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static java.lang.Thread.sleep;
+import static java.util.Objects.requireNonNull;
+import static org.springframework.http.HttpStatus.NOT_MODIFIED;
+import static org.springframework.http.HttpStatus.OK;
+import static reactor.core.publisher.Mono.just;
 
 @RestController
 @RequestMapping(path = "/payment-api", consumes = MediaType.APPLICATION_JSON_VALUE)
 public class PaymentController
 {
     private static final Logger LOG = LoggerFactory.getLogger(PaymentController.class);
+
+    @Value("${data-api.baseUrl}")
+    private String dataApiUrl;
 
     @GetMapping
     public HashMap<String, String> welcome()
@@ -28,10 +43,38 @@ public class PaymentController
     }
 
     @PostMapping(value = "/pay")
-    public String pay(@RequestBody PaymentRequest paymentRequest)
+    public ResponseEntity<?> pay(@RequestBody PaymentRequest paymentRequest) throws InterruptedException
     {
-        // TODO - simulate external service payment call and callback
-        LOG.info("Requested to move PENDING to PAID for checkoutId: {}", paymentRequest.getCheckoutId());
-        return "NOT IMPLEMENTED YET";
+        // Random payment process
+        int waitForPay = ThreadLocalRandom.current().nextInt(5);
+        boolean paid = ThreadLocalRandom.current().nextBoolean();
+
+        // Fetch checkout
+        WebClient webClient = WebClient.create(dataApiUrl);
+        Checkout checkout = webClient.get()
+                                     .uri("/checkouts/" + paymentRequest.getCheckoutId())
+                                     .retrieve()
+                                     .bodyToMono(Checkout.class)
+                                     .block();
+
+        // simulate successful payment
+        sleep(waitForPay * 1000);
+        if (paid)
+        {
+            // update
+            requireNonNull(checkout).setState("PAID");
+            Checkout updatedCheckout = webClient.put()
+                                                .uri("/checkouts/" + paymentRequest.getCheckoutId())
+                                                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                                                .body(just(checkout), Checkout.class)
+                                                .retrieve()
+                                                .bodyToMono(Checkout.class)
+                                                .block();
+            LOG.info("Successfully PAID checkout id {} with data: {}", paymentRequest.getCheckoutId(), updatedCheckout);
+            return new ResponseEntity<>(updatedCheckout, OK);
+        }
+
+        LOG.error("Unsuccessful payment for checkout id {} with data: {}", paymentRequest.getCheckoutId(), checkout);
+        return new ResponseEntity<>(checkout, NOT_MODIFIED);
     }
 }
